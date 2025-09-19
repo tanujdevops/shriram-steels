bcc8312de46d5b2b9ff226ae0242c8ab7cbe1fe6
Tanuj Sharma <109404543+tanujdevops@users.noreply.github.com>
2025-09-19 15:27:27 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -70,6 +70,7 @@ export default function HomePage() {
                 alt="Shriram Steels Logo"
                 width={120}
                 height={36}
+                className="h-6 w-auto"
                 priority
               />
             </Link>
